e33737630fe86180057dca4a357a4e0d979ec4a1
Carsten <carstensandmadsen@gmail.com>
2025-09-20 23:56:45 +0200

    RPM and clutch working now - Clutch a bit weird, maybe

--- GenericTelemetryProvider/WreckfestTelemetryProvider.cs
+++ GenericTelemetryProvider/WreckfestTelemetryProvider.cs
@@ -17,9 +17,13 @@ namespace GenericTelemetryProvider
 {
     class WreckfestTelemetryProvider : GenericProviderBase
     {
-        Int64 memoryAddress;
+        Int64 memoryAddressMatrix;
         Int64 memoryAddressGear;
-        public static byte[] gear = new byte[1] {4};
+        Int64 memoryAddressRpm;
+        Int64 memoryAddressClutch;
+        sbyte gear = 0;
+        float rpm = 0.0f;
+        float clutch = 0.0f;
         Thread t;
         Process mainProcess = null;
 
@@ -56,7 +60,7 @@ namespace GenericTelemetryProvider
 
             RegularMemoryScan scan = new RegularMemoryScan(mainProcess, lStart, 140737488355327); //32gig            scan.ScanProgressChanged += new RegularMemoryScan.ScanProgressedEventHandler(scan_ScanProgressChanged);
             scan.ScanProgressChanged += new RegularMemoryScan.ScanProgressedEventHandler(scan_ScanProgressChanged);
-            scan.ScanCompleted += new RegularMemoryScan.ScanCompletedEventHandler(scan_ScanCompleted);
+            scan.ScanCompleted += new RegularMemoryScan.ScanCompletedEventHandler(scan_ScanCompleted);;
             scan.ScanCanceled += new RegularMemoryScan.ScanCanceledEventHandler(scan_ScanCanceled);
             string scanString = "carRootNode" + vehicleString;
             scan.StartScanForString(scanString, 1);
@@ -80,18 +84,28 @@ namespace GenericTelemetryProvider
         {
             ProcessMemoryReader reader = new ProcessMemoryReader();
             reader.ReadProcess = mainProcess;
-            UInt64 readSize = 4 * 4 * 4;
-            byte[] readBuffer = new byte[readSize];
-            byte[] lastReadBuffer = new byte[readSize];
             reader.OpenProcess();
+
+            UInt64 readSizeMatrix = 4 * 4 * 4;
+            byte[] readBufferMatrix = new byte[readSizeMatrix];
+            byte[] lastReadBufferMatrix = new byte[readSizeMatrix];
             
-            ProcessMemoryReader readerGear = new ProcessMemoryReader();
-            readerGear.ReadProcess = mainProcess;
             UInt64 readSizeGear = 1;
             byte[] readBufferGear = new byte[readSizeGear];
             byte[] lastReadBufferGear = new byte[readSizeGear];
-            readerGear.OpenProcess();
-            
+
+            UInt64 readSizeRpm = 4;
+            byte[] readBufferRpm = new byte[readSizeRpm];
+            byte[] lastReadBufferRpm = new byte[readSizeRpm];
+
+            UInt64 readSizeClutch = 4;
+            byte[] readBufferClutch = new byte[readSizeClutch];
+            byte[] lastReadBufferClutch = new byte[readSizeClutch];
+
+            UInt64 readSizeAll = readSizeMatrix + readSizeGear + readSizeRpm + readSizeClutch;
+            //byte[] readBufferAll = new byte[readSizeAll];
+            byte[] lastReadBufferAll = new byte[readSizeAll];
+
 
             float frameRateSecs = 1.0f / 60.0f;
 
@@ -105,23 +119,31 @@ namespace GenericTelemetryProvider
                 try
                 {
                     Matrix4x4 transform = Matrix4x4.Identity;
-                    Int64 byteReadSize;
+                    Int64 byteReadSizeMatrix;
                     Int64 byteReadSizeGear;
+                    Int64 byteReadSizeRpm;
+                    Int64 byteReadSizeClutch;
+                    Int64 byteReadSizeAll;
                     bool different = false;
                     do
                     {
                         //read
-                        reader.ReadProcessMemory((IntPtr)memoryAddress, readSize, out byteReadSize, readBuffer);
+                        reader.ReadProcessMemory((IntPtr)memoryAddressMatrix, readSizeMatrix, out byteReadSizeMatrix, readBufferMatrix);
+                        reader.ReadProcessMemory((IntPtr)memoryAddressGear, readSizeGear, out byteReadSizeGear, readBufferGear);
+                        reader.ReadProcessMemory((IntPtr)memoryAddressRpm, readSizeRpm, out byteReadSizeRpm, readBufferRpm);
+                        reader.ReadProcessMemory((IntPtr)memoryAddressClutch, readSizeClutch, out byteReadSizeClutch, readBufferClutch);
 
-                        if (byteReadSize == 0)
+                        byteReadSizeAll = byteReadSizeMatrix + byteReadSizeGear + byteReadSizeRpm + byteReadSizeClutch;
+                        if (byteReadSizeAll == 0)
                         {
                             continue;
                         }
 
+                        byte[] readBufferAll = readBufferMatrix.Concat(readBufferGear).Concat(readBufferRpm).Concat(readBufferClutch).ToArray();
                         //check if different
-                        for (int i = 0; i < (int)readSize; ++i)
+                        for (int i = 0; i < (int)readSizeAll; ++i)
                         {
-                            if (readBuffer[i] != lastReadBuffer[i])
+                            if (readBufferAll[i] != lastReadBufferAll[i])
                             {
                                 different = true;
                                 break;
@@ -137,27 +159,45 @@ namespace GenericTelemetryProvider
 
 
                     //read transform
-                    reader.ReadProcessMemory((IntPtr)memoryAddress, readSize, out byteReadSize, readBuffer);
-                    readerGear.ReadProcessMemory((IntPtr)memoryAddressGear, readSizeGear, out byteReadSizeGear, readBufferGear);
+                    // Read current values
+                    reader.ReadProcessMemory((IntPtr)memoryAddressMatrix, readSizeMatrix, out byteReadSizeMatrix, readBufferMatrix);
+                    reader.ReadProcessMemory((IntPtr)memoryAddressGear, readSizeGear, out byteReadSizeGear, readBufferGear);
+                    reader.ReadProcessMemory((IntPtr)memoryAddressRpm, readSizeRpm, out byteReadSizeRpm, readBufferRpm);
+                    reader.ReadProcessMemory((IntPtr)memoryAddressClutch, readSizeClutch, out byteReadSizeClutch, readBufferClutch);
 
-                    if (byteReadSize == 0)
+                    byteReadSizeAll = byteReadSizeMatrix + byteReadSizeGear + byteReadSizeRpm + byteReadSizeClutch;
+                    if (byteReadSizeAll == 0)
                     {
                         Console.WriteLine("REEAAALLY DONT WANT THIS TO HAPPEN");
                         continue;
                     }
 
-                    Buffer.BlockCopy(readBuffer, 0, lastReadBuffer, 0, readBuffer.Length);
-                    Buffer.BlockCopy(readBufferGear, 0, lastReadBufferGear, 0, readBufferGear.Length);
+                    // Copy current values for next difference check
+                    Buffer.BlockCopy(readBufferMatrix, 0, lastReadBufferAll, 0, readBufferMatrix.Length);
+                    Buffer.BlockCopy(readBufferGear, 0, lastReadBufferAll, readBufferMatrix.Length, readBufferGear.Length);
+                    Buffer.BlockCopy(readBufferRpm, 0, lastReadBufferAll, readBufferMatrix.Length + readBufferGear.Length, readBufferRpm.Length);
+                    Buffer.BlockCopy(readBufferClutch, 0, lastReadBufferAll, readBufferMatrix.Length + readBufferGear.Length + readBufferRpm.Length, readBufferClutch.Length);
 
-                    float[] floats = new float[4 * 4];
+                    // Get matrix value
+                    float[] matrixFloats = new float[4 * 4];
+                    Buffer.BlockCopy(readBufferMatrix, 0, matrixFloats, 0, readBufferMatrix.Length);
+                    Matrix4x4 newTransform = new Matrix4x4(matrixFloats[0], matrixFloats[1], matrixFloats[2], matrixFloats[3]
+                                , matrixFloats[4], matrixFloats[5], matrixFloats[6], matrixFloats[7]
+                                , matrixFloats[8], matrixFloats[9], matrixFloats[10], matrixFloats[11]
+                                , matrixFloats[12], matrixFloats[13], matrixFloats[14], matrixFloats[15]);
 
-                    Buffer.BlockCopy(readBuffer, 0, floats, 0, readBuffer.Length);
-                    Buffer.BlockCopy(readBufferGear, 0, WreckfestTelemetryProvider.gear, 0, readBufferGear.Length);
-
-                    Matrix4x4 newTransform = new Matrix4x4(floats[0], floats[1], floats[2], floats[3]
-                                , floats[4], floats[5], floats[6], floats[7]
-                                , floats[8], floats[9], floats[10], floats[11]
-                                , floats[12], floats[13], floats[14], floats[15]);
+                    // Get gear value
+                    byte[] gearBytes = new byte[1];
+                    Buffer.BlockCopy(readBufferGear, 0, gearBytes, 0, readBufferGear.Length);
+                    gear = (sbyte)gearBytes[0];
+                    // Get rpm value
+                    float[] rpmFloats = new float[1];
+                    Buffer.BlockCopy(readBufferRpm, 0, rpmFloats, 0, readBufferRpm.Length);
+                    rpm = rpmFloats[0];
+                    // Get clutch value
+                    float[] clutchFloats = new float[1];
+                    Buffer.BlockCopy(readBufferClutch, 0, clutchFloats, 0, readBufferClutch.Length);
+                    clutch = clutchFloats[0];
 
                     ProcessTransform(newTransform, frameRateSecs);
 
@@ -169,7 +209,6 @@ namespace GenericTelemetryProvider
 
             }
             reader.CloseHandle();
-            readerGear.CloseHandle();
 
             StopSending();
 
@@ -192,8 +231,23 @@ namespace GenericTelemetryProvider
         public override void SimulateEngine()
         {
             base.SimulateEngine();
-            rawData.gear = (float)(sbyte)WreckfestTelemetryProvider.gear[0]; //2.0f;//gear[1];
-            rawData.max_gears = 3.0f;
+
+            rawData.gear = gear; //2.0f;//gear[1];
+            rawData.max_gears = 6.0f;
+
+            rawData.idle_rpm = 1100.0f;
+            rawData.max_rpm = 7200.0f;
+            //rawData.engine_rate = (rpm - 1100) / (7200 - 1100);
+
+            rawData.clutch_input = clutch;
+        }
+
+        public override void ProcessInputs()
+        {
+            base.ProcessInputs();
+
+            rawData.engine_rate = rpm;
+            filteredData.engine_rate = rpm;
         }
 
         void scan_ScanProgressChanged(object sender, ScanProgressChangedEventArgs e)
@@ -216,30 +270,18 @@ namespace GenericTelemetryProvider
 
                 return;
             }
-            Utils.DebugLog("Found gearPattern at: " + e.MemoryAddresses[0].ToString("X"));
-            Utils.DebugLog("Found gear1 at: " + (e.MemoryAddresses[0] + 0x1C).ToString("X"));
-            
-            Utils.DebugLog("Found gear2 at: " + (e.MemoryAddresses[0] + 28).ToString("X"));
+            Utils.DebugLog("Wreckfest - Found gear, rpm, clutch pattern at: " + e.MemoryAddresses[0].ToString("X"));
 
             //            memoryAddress = e.MemoryAddresses[0] - ((4 * 4 * 4) + 4); //offset backwards from found address to start of matrix
             //            memoryAddress = e.MemoryAddresses[0] - ((4 * 4 * 4) + 8); //offset backwards from found address to start of matrix
-            memoryAddressGear = e.MemoryAddresses[0] + 28;// (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
-
+            memoryAddressGear = e.MemoryAddresses[0] + 0x1C;// (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
+            Utils.DebugLog("Wreckfest - Found gear value at: " + memoryAddressGear.ToString("X"));
+            memoryAddressRpm = e.MemoryAddresses[0] - 0x2DC;// (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
+            Utils.DebugLog("Wreckfest - Found rpm value at: " + memoryAddressRpm.ToString("X"));
+            memoryAddressClutch = e.MemoryAddresses[0] + 0x0C;// (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
+            Utils.DebugLog("Wreckfest - Found clutch value at: " + memoryAddressClutch.ToString("X"));
+            
             ui.StatusTextChanged("Success2");
-
-            ProcessMemoryReader readerGear = new ProcessMemoryReader();
-            readerGear.ReadProcess = mainProcess;
-            UInt64 readSizeGear = 1;            
-            Int64 byteReadSizeGear;
-            byte[] readBufferGear = new byte[readSizeGear];
-            byte[] lastReadBufferGear = new byte[readSizeGear];
-            readerGear.OpenProcess();
-            readerGear.ReadProcessMemory((IntPtr)memoryAddressGear, readSizeGear, out byteReadSizeGear, readBufferGear);
-            Buffer.BlockCopy(readBufferGear, 0, lastReadBufferGear, 0, readBufferGear.Length);
-            byte[] mygear = new byte[] {5};
-            Buffer.BlockCopy(readBufferGear, 0, mygear, 0, readBufferGear.Length);
-            Utils.DebugLog("Gear value: " + ((sbyte)mygear[0]).ToString("X"));
-            readerGear.CloseHandle();
 
             t = new Thread(ScanComplete);
             t.IsBackground = true;
@@ -257,9 +299,12 @@ namespace GenericTelemetryProvider
                 return;
             }
 
+            Utils.DebugLog("Wreckfest - Found matrix pattern at: " + e.MemoryAddresses[0].ToString("X"));
+            
             //            memoryAddress = e.MemoryAddresses[0] - ((4 * 4 * 4) + 4); //offset backwards from found address to start of matrix
             //            memoryAddress = e.MemoryAddresses[0] - ((4 * 4 * 4) + 8); //offset backwards from found address to start of matrix
-            memoryAddress = e.MemoryAddresses[0] - (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
+            memoryAddressMatrix = e.MemoryAddresses[0] - (((4 * 4 * 4) * 2) + 8); //offset backwards from found address to start of matrix
+            Utils.DebugLog("Wreckfest - Found matrix value at: " + memoryAddressMatrix.ToString("X"));
 
             ui.StatusTextChanged("Success1");
 /*
